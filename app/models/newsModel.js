c6c8042de574069ee839be8baa2a215c68b92bd4
Marcos Vinícius Queiroz <49909597+Kieroth29@users.noreply.github.com>
2022-07-11 17:01:17 -0300

    Improving recent articles logic

--- app/models/newsModel.js
+++ app/models/newsModel.js
@@ -10,7 +10,18 @@ NewsDAO.prototype.getArticles = function(connection, callback){
 }
 
 NewsDAO.prototype.getRecentArticles = function(connection, id, callback){
-    connection.query("SELECT * from noticias where id = " + id + " - 1 or id = " + id + "+ 1;", callback);
+    connection.query(`
+        SELECT * FROM (
+            SELECT * FROM noticias WHERE id > ` + id + ` ORDER BY id LIMIT 1
+        ) x
+      
+        UNION ALL
+      
+        SELECT * FROM (
+            SELECT * FROM noticias WHERE id < ` + id + ` ORDER BY id DESC LIMIT 1
+        ) y
+        
+        ORDER BY id;`, callback);
 }
 
 NewsDAO.prototype.setArticle = function(connection, article, callback){
